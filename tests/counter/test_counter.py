7cca137c00b8cca12d31843dedd9f05f1c7ce814
DearJao <jvvr2013@gmail.com>
2023-03-01 15:14:54 -0300

    teste test_counter implementado

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,14 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+    word = "python"
+
+    result = count_ocurrences(path, word)
+
+    assert result == 1639
+
+    assert count_ocurrences(path, "PYTHON") == 1639
+    assert count_ocurrences(path, "Python") == 1639
+    assert count_ocurrences(path, "pYThOn") == 1639
